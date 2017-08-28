d14089eed2c6a593d05baa3e8ac5bd1ce339f045
daddinuz <daddinuz@gmail.com>
2017-08-28 19:18:07 +0200

    Update setup.py
    New version 0.1.1 with equal support.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_packages(path):
 packages = get_packages('value_object')
 setup(
     name='value_object',
-    version='0.1.0',
+    version='0.1.1',
     description='Value Object',
     url='https://github.com/daddinuz/value_object',
     author='daddinuz',
